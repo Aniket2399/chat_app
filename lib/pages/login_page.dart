440c74252c3fc76e2db7d465eef60d505eb40326
Aniket2399 <kshirsagarask19@gmail.com>
2024-08-27 01:31:15 -0400

    complete chat app

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/my_buttons.dart';
+import 'package:flutter_application_1/components/my_text_field.dart';
+import 'package:flutter_application_1/services/auth/auth_service.dart';
+import 'package:provider/provider.dart';
+
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+  const LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordcontroller = TextEditingController();
+
+  void signIn() async {
+    final authService = Provider.of<AuthService>(context, listen: false);
+
+    try {
+      await authService.signInWithEmailandPaswword(
+          emailController.text, passwordcontroller.text);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                Icon(
+                  Icons.message,
+                  size: 100,
+                  color: Colors.grey[800],
+                ),
+                const SizedBox(height: 50),
+                const Text(
+                  "Welcome back",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: passwordcontroller,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+                MyButton(onTap: signIn, text: "Sign In"),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Not a member?'),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Register Now',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
